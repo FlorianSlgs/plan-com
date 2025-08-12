94b47b38b3252468cf4e66f8e116eaa87d4d76a1
Florian Salengros <florian.salengros@gmail.com>
2025-08-12 22:39:38 +0200

    types jasmine accepté

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router'; // Importer RouterOutlet et RouterLink
+import { RouterOutlet } from '@angular/router';
 import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
-  standalone: true, // Marquer comme standalone
-  imports: [RouterOutlet], // Importer les directives de routing nécessaires
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-auth-app-standalone';
+  title = 'plan-com';
   // Injecter AuthService pour l'utiliser dans le template
   private authService = inject(AuthService);
 
