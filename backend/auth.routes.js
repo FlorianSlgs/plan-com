d56e3515343ee34c93b5d1d60b21d867f9779095
Florian Salengros <florian.salengros@gmail.com>
2025-08-28 12:07:39 +0200

    ajout règle secure, domain et samesite

--- backend/auth.routes.js
+++ backend/auth.routes.js
@@ -120,8 +120,9 @@ module.exports = (pool) => {
       // Ajoute le token dans un cookie HTTP Only
       res.cookie('authToken', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production', // true en prod (HTTPS)
-        sameSite: 'strict',
+        secure: process.env.SECURE,
+        sameSite: process.env.SAMESITE,
+        domain: process.env.DOMAIN,
         maxAge: 7 * 24 * 60 * 60 * 1000 // 7 jours
       });
 
