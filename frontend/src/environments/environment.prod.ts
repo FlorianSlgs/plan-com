0685814908b5c556a2d7f868dfa1fe2f1d8d8462
Florian Salengros <florian.salengros@gmail.com>
2025-08-28 17:06:16 +0200

    build

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  baseUrl: 'https://plancomserver.duckdns.org/api',
+  baseUrl: 'https://plancomserver.duckdns.org/api/api',
   endpoints: {
     auth: '/auth',
     header: '/header',
